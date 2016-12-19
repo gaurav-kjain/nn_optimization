0a8ad13d8d0c62498e7a378d61ad1786379217c8
gaurav-kjain <gaurav.kjain@gmail.com>
2016-12-19 14:51:49 +0530

    new changes to run all

--- RunConfig.py
+++ RunConfig.py
@@ -3,13 +3,5 @@ sys.path.append("dnn_optimization")
 from ModelRunner import *
 db="MNIST"
 bmode="outliers_iqr_npass"
-run_experiment_config(db, bmode=bmode,maindir="/home/gaurav.kjain/Thesis/Results",bmult=4,verbose=1)
-
-
-from ModelRunner import *
-db="CIFAR10"
-bmode="outliers_iqr_npass"
-run_experiment_config(db, bmode=bmode,maindir="/home/gauravkj/Thesis/Results",bmult=4,verbose=1)
-
-
-pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+#run_experiment_config(db, bmode=bmode,maindir="/home/gaurav.kjain/Thesis/Results",bmult=4,verbose=1)
+run_all_config(db, maindir="/home/gaurav.kjain/Thesis/Results",bmult=4,verbose=1)
