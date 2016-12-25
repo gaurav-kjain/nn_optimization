ae33d43624f91a2b92b4ffd28be7cb826fa4cc89
unknown <gaurav.kjain@gmail.com>
2016-12-25 19:33:45 +0530

    adding save config

---
+++ SaveConfig.py
@@ -0,0 +1,7 @@
+import sys
+#sys.path.append("nn_optimization")
+from ModelPlotter import *
+db="MNIST"
+bmode="outliers_iqr_npass"
+#save_all_config(maindir="../Results_outliers_e-2",bmult=4,verbose=1)
+save_csv_config(db=db,bmode=bmode,maindir="../Results_outliers_e-2",bmult=4,verbose=1)
